61b287dd68a32c1b631bbc11fa5ba7f27972c1a5
ZIFENG278 <zifengzhang18@gmail.com>
2024-01-23 15:46:53 +0800

    run.sh default use sophon-opencv and delete x86 tpu_perf

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
-export PYTHONPATH=$PYTHONPATH:/opt/sophon/sophon-opencv-latest/opencv-python/
+export LD_LIBRARY_PATH=/opt/sophon/libsophon-current/lib:$LD_LIBRARY_PATH
+export PYTHONPATH=/opt/sophon/sophon-opencv-latest/opencv-python/:$PYTHONPATH
 python3 gr.py
